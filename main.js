29cc1cef12dd58cde7403b3fe2e6f8ee2153fee8
codecommit-test-user <register@yuyakawai.com>
2023-12-08 18:50:46 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,187 @@
+// キャンバスと描画コンテキストを取得
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+// ボールの初期位置と半径
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let ballRadius = 8;
+
+// ボールの動き
+let dx = (Math.random() - 0.5) * 10;
+let dy = -2;
+
+// パドルの高さ、幅、位置
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+
+// パドルの動き
+let rightPressed = false;
+let leftPressed = false;
+
+// ブロックの設定
+let brickRowCount = 3;
+let brickColumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+
+// スコア
+let score = 0;
+
+// ブロックの配列を作成
+let bricks = [];
+for (let c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for (let r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
+  }
+}
+
+// キーイベントのハンドラー
+const keyDownHandler = (e) => {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = true;
+  } else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = true;
+  }
+};
+
+const keyUpHandler = (e) => {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = false;
+  } else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = false;
+  }
+};
+
+// キーイベントのリスナーを設定
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+// タッチイベントのハンドラー
+const touchStartHandler = (e) => {
+  let relativeX = e.touches[0].clientX - canvas.offsetLeft;
+  if (relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth / 2;
+  }
+};
+
+const touchMoveHandler = (e) => {
+  let relativeX = e.touches[0].clientX - canvas.offsetLeft;
+  if (relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth / 2;
+  }
+};
+
+// タッチイベントのリスナーを設定
+canvas.addEventListener("touchstart", touchStartHandler, false);
+canvas.addEventListener("touchmove", touchMoveHandler, false);
+
+// 衝突検出
+const collisionDetection = () => {
+  for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      let b = bricks[c][r];
+      if (b.status == 1) {
+        if (
+          x > b.x &&
+          x < b.x + brickWidth &&
+          y > b.y &&
+          y < b.y + brickHeight
+        ) {
+          dy = -dy;
+          b.status = 0;
+          score++;
+          if (score == brickRowCount * brickColumnCount) {
+            document.getElementById("gameMessage").innerText =
+              "YOU WIN, CONGRATULATIONS!";
+            clearInterval(interval); // Needed for Chrome to end game
+          }
+        }
+      }
+    }
+  }
+};
+
+// ボールを描画
+const drawBall = () => {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+};
+
+// パドルを描画
+const drawPaddle = () => {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+};
+
+// ブロックを描画
+const drawBricks = () => {
+  for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      if (bricks[c][r].status == 1) {
+        let brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+        let brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
+    }
+  }
+};
+
+// スコアを描画
+const drawScore = () => {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("Score: " + score, 8, 20);
+};
+
+// ゲームを描画
+const draw = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBricks();
+  drawBall();
+  drawPaddle();
+  drawScore();
+  collisionDetection();
+
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+  if (y + dy < ballRadius) {
+    dy = -dy;
+  } else if (y + dy > canvas.height - ballRadius) {
+    if (x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy;
+    } else {
+      document.getElementById("gameMessage").innerText = "GAME OVER";
+      clearInterval(interval); // Needed for Chrome to end game
+    }
+  }
+
+  if (rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 7;
+  } else if (leftPressed && paddleX > 0) {
+    paddleX -= 7;
+  }
+
+  x += dx;
+  y += dy;
+};
+
+let interval = setInterval(draw, 10);
